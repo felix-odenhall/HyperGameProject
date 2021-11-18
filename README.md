ae21a5940131ace67e903d2f6161107129061c6f
Felix-Navidad <90831955+Felix-Navidad@users.noreply.github.com>
2021-11-18 14:12:21 +0100

    added shots

--- README.md
+++ README.md
@@ -5,3 +5,29 @@ npm install
 
 Step2
 npm run start
+
+-- PSUEDO CODE --
+
+Add one more orc
+
+Create a sprite for the "shot/bullet"
+
+MAke Gandalf shoot on click, spacebar
+
+Set a timeout between shots
+
+shot should go in the direction Gandalf is facing
+
+bullets should have a set speed
+
+shots should be removed from the game when hit world bounds
+
+collision between bullet and orcs,
+
+orcs should disapear when hit
+
+display a text with score
+
+You get a point when hitting orcs
+
+increment the points
--- Scripts/game.js
+++ Scripts/game.js
@@ -3,7 +3,9 @@ import Phaser from "phaser";
 
 const gameState = {
 
+    gandalfSpeed: 2
 };
+
 
 export default class Game extends Phaser.Scene {
     
@@ -17,16 +19,26 @@ export default class Game extends Phaser.Scene {
             "gandalf",
             "https://content.codecademy.com/courses/learn-phaser/physics/codey.png"
             );
+        this.load.image(
+            "shot",
+            "https://content.codecademy.com/courses/learn-phaser/physics/bug_2.png"
+            );
+
     }
+
     
     create ()
     {
-        gameState.gandalf = this.physics.add.sprite(200, 200, 'gandalf')
+
+        gameState.gandalf = this.physics.add.sprite(600, 500, 'gandalf')
         .setCollideWorldBounds(true);
     
         gameState.orc = this.physics.add.sprite(600, 300, 'orc')
         .setImmovable() 
-    
+
+        gameState.orc2 = this.physics.add.sprite(600, 200, 'orc')
+        .setImmovable() 
+
         this.physics.add.collider(gameState.gandalf, gameState.orc, () => {
             this.physics.pause()
             this.add.text(400, 300, 'GAME OVER', { fontSize: "32px", fill: "#ffffff"})
@@ -37,22 +49,41 @@ export default class Game extends Phaser.Scene {
 
         gameState.cursors = this.input.keyboard.createCursorKeys();
 
-
+        gameState.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    
     }
+
     
     update ()
     {
+
+        // function  createShot(){
+        //     const shots = this.physics.add.group();
+        //     shots.create(600, 500, 'shot')
+        // }
+        
         if (gameState.cursors.up.isDown){
-            gameState.gandalf.setVelocityY(-200);
+            // gameState.gandalf.setVelocityY(-200);
+            gameState.gandalf.y -= gameState.gandalfSpeed
         } else if (gameState.cursors.down.isDown){
-            gameState.gandalf.setVelocityY(200);
+            gameState.gandalf.y += gameState.gandalfSpeed
         } else {gameState.gandalf.setVelocityY(0)}
         if (gameState.cursors.left.isDown){
-            gameState.gandalf.setVelocityX(-200);
+            gameState.gandalf.x -= gameState.gandalfSpeed
         } else if (gameState.cursors.right.isDown){
-            gameState.gandalf.setVelocityX(200);
+            gameState.gandalf.x += gameState.gandalfSpeed
         } else {gameState.gandalf.setVelocityX(0)}
+        if (Phaser.Input.Keyboard.JustDown(gameState.spacebar)){
+            gameState.shots = this.physics.add.group();
+            gameState.shots.create(gameState.gandalf.x, gameState.gandalf.y, 'shot')
+            gameState.shots.setVelocityY(-200)
+            this.physics.add.collider(gameState.orc, gameState.shots, function (orc, shots){
+                orc.destroy()
+                shots.destroy()
+            })
+        }
     }
+    
 
 
 }
