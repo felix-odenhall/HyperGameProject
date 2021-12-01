0abcee96a2fd274e41d3be765939a988bd5884b9
iseneld <84731866+iseneld@users.noreply.github.com>
2021-12-01 14:32:01 +0100

    High score is now saved to local storage.

--- Scripts/game.js
+++ Scripts/game.js
@@ -65,6 +65,11 @@ export default class Game extends Phaser.Scene {
     this.load.audio("dead", dead);
 
     this.load.audio("shoot", shoot);
+
+    // If a high score is stored in localStorage, it will replace the gameState.highScore array.
+    if (localStorage.getItem("highScores")) {
+      gameState.highScore = JSON.parse(localStorage.getItem("highScores"));
+    }
   }
 
   create() {
@@ -260,6 +265,11 @@ export default class Game extends Phaser.Scene {
         document.querySelector("h1").setAttribute("style", "color: inherit");
         document.querySelector("h1").innerText = "Name of the Game";
         document.querySelector("h3").remove();
+
+        // Updates the high score board, if a high score has ever been saved.
+        if (localStorage.getItem("highScores")) {
+          gameState.highScores = JSON.parse(localStorage.getItem("highScores"));
+        }
         this.restartGame();
       });
       return;
@@ -296,10 +306,7 @@ export default class Game extends Phaser.Scene {
     // SHOT MOVEMENT DIRECTION
 
     gameState.shot.setVelocity(x, y);
-
     gameState.gandalf.anims.play("shoot", true);
-
-    console.log("hello");
   }
 
   // ADDING ORCS
@@ -383,7 +390,6 @@ export default class Game extends Phaser.Scene {
 
   addToHighScore = function (score) {
     const min = Math.min(...gameState.highScore);
-    console.log(min);
 
     if (score > min) {
       gameState.highScore.sort(function (a, b) {
@@ -412,6 +418,8 @@ export default class Game extends Phaser.Scene {
         ol.appendChild(li);
         console.log("Done");
       }
+      // Makes a string of the sorted high score array and saves it in localStorage
+      localStorage.setItem("highScores", JSON.stringify(sortedHighScore));
     }
     return;
   };
